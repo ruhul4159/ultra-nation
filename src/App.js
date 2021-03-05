3f933e259aecc7a8a02669f2e27d28a44ea37fd6
ruhulamin87 <ruhul4159@gmail.com>
2021-03-05 23:07:50 +0300

    ok

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Country from './Components/Country/Country';
+import Cart from './Components/Cart/Cart';
 
 function App() {
+const [ countries, setCountries] = useState([]);
+const [cart, setCart]= useState([]);
+  
+  useEffect(() => {
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(data=>setCountries(data))
+    .catch(error => console.log(error))
+  }, [])
+
+  const handleAddCountry = (country) => {
+   const newCart = [...cart, country];
+   setCart(newCart);
+  }
+
   return (
     <div className="App">
+      <h2>Countries loaded: {countries.length}</h2>
+      <h4>Country Added: {cart.length}</h4>
+      <Cart cart={cart}></Cart>
+        {
+        countries.map(country => <Country country={country} handleAddCountry={handleAddCountry} key={country.alpha3Code}></Country>)
+        }
+      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/Components/Cart/Cart.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Cart = (props) => {
+    const cart = props.cart;
+//    let totalPopulation = 0;
+//     for (let i = 0; i < cart.length; i++) {
+//         const country = cart[i];
+//         totalPopulation = totalPopulation + country.population;
+//     }
+    const totalPopulation = cart.reduce((sum, country) => sum + country.population, 0)
+        
+   
+    return (
+        <div>
+            <h5>This is Cart: {cart.length}</h5>
+            <p>total population: {totalPopulation}</p>
+        </div>
+    );
+};
+
+export default Cart;
